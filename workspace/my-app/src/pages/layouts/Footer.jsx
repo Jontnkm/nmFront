0b865b896869ab889f4d835698f9805ef4568d60
Jonathan Kim <101095622+Jontnkm@users.noreply.github.com>
2025-05-09 22:33:26 +0900

    2025.05.09 야간 작업

--- workspace/my-app/src/pages/layouts/Footer.jsx
+++ workspace/my-app/src/pages/layouts/Footer.jsx
@@ -9,18 +9,19 @@ function Footer() {
                     <p>
                         <span>세무회계 낭만</span>
                         <span>대표 <em>고수일</em></span>
+                        <span>사업자번호 <em>166-12-02280</em></span>
+                    </p>
+                    <p className={st.address}>
+                        <span><em>주소</em><em>서울특별시 금천구 디지털로 178, A동 506호(현대 지식산업센터 가산 퍼블릭)</em></span>
                     </p>
                     <p>
-                        <span>주소 <em>서울특별시 강남구 테헤란로 70길 15, 1층 (낭만빌딩)</em></span>
+                        <span>대표번호 <em>070-4077-4374</em></span>
                     </p>
                     <p>
-                        <span>대표번호 <em>000-0000-0000</em></span>
-                    </p>
-                    <p>
-                        <span>이메일 <em>nangman@naver.com</em></span>
+                        <span>이메일 <em>nangmantax@daum.net</em></span>
                     </p>
                     <p className={st.cpryt}>
-                        Copyright ⓒ STANDARD All Rights Reserved.
+                        Copyright ⓒ NangMan All Rights Reserved.
                     </p>
                 </div>
                 <div className={st.right}>
@@ -36,6 +37,20 @@ function Footer() {
                             <a href="javascript:void(0);">개인정보처리방침</a>
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+        
+        <div className={st.cptQuick}>
+            <div className={st.inner}>
+                <div className={st.quickList}>
+                    <ul>
+                        <li><a href="javascript:void(0)" className={st.consult}><span className="hidden">상담하기</span></a></li>
+                        <li><a href="javascript:void(0)" className={st.naver}><span className="hidden">네이버블로그</span></a></li>
+                        <li><a href="javascript:void(0)" className={st.kakao}><span className="hidden">카카오채널</span></a></li>
+                        <li><a href="javascript:void(0)" className={st.instagram}><span className="hidden">인스타그램</span></a></li>
+                        <li><a href="javascript:void(0)" className={st.top}><span className="hidden"></span></a></li>
+                    </ul>
                 </div>
             </div>
         </div>
--- workspace/my-app/src/pages/special/Education.jsx
+++ workspace/my-app/src/pages/special/Education.jsx
@@ -1,7 +1,115 @@
+import HeaderSub from "../layouts/HeaderSub";
+import Footer from "../layouts/Footer";
+
+import lyt from '../modules/lyt.module.scss';
+import style from '../modules/education.module.scss';
+import comm from '../modules/comm.module.scss';
+
 function Education() {
     return <>
-        <h1>Education page</h1>
-        <div>test</div>
+        <HeaderSub></HeaderSub>
+        <div className={lyt.wrap}>
+            <div className={lyt.container}>
+                <div className={lyt.subLayout}>
+                    <div className={comm.subPageTitle}>
+                        <p className={comm.pageTit}>
+                            학원/교육업
+
+                            <span className={comm.pageDesc}>
+                                단순한 세무 대행을 넘어, 비즈니스의 성장을 함께 설계하는 파트너가 되겠습니다.<br/>
+                                고객의 내일을 함께 준비하는 든든한 조력자가 되어드리겠습니다.
+                            </span>
+                        </p>
+                        <div className={comm.breadCrumb}>
+                            <ul>
+                                <li>
+                                    <a href="javascript:void(0)">
+                                        <img src="/images/common/homeBtn.svg" alt="" />
+                                    </a>
+                                </li>
+                                <li className={comm.depth01}>
+                                    <a href="javascript:void(0)">낭만에 대하여</a>
+                                </li>
+                                <li className={comm.depth02}>
+                                    <a href="javascript:void(0)">학원/교육업</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className={comm.contents}>
+                        <div className={comm.inner}>
+                            <div className={style.cptTable}>
+                                <table className={style.tbData}>
+                                    <colgroup>
+                                        <col width="10%"/>
+                                        <col width="auto"/>
+                                        <col width="15%"/>
+                                        <col width="15%"/>
+                                    </colgroup>
+                                    <thead>
+                                        <tr>
+                                            <th>번호</th>
+                                            <th className={style.left}>제목</th>
+                                            <th>작성자</th>
+                                            <th>작성일</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td className={style.left}>학원업 사업자등록 주요사항</td>
+                                            <td>홍길동</td>
+                                            <td>2025-01-23</td>
+                                        </tr>
+                                        <tr>
+                                            <td>2</td>
+                                            <td className={style.left}>학원업 강사고용 유의사항</td>
+                                            <td>홍길동</td>
+                                            <td>2025-01-23</td>
+                                        </tr>
+                                        <tr>
+                                            <td>3</td>
+                                            <td className={style.left}>학원업 교육청 연계 세무사항</td>
+                                            <td>홍길동</td>
+                                            <td>2025-01-23</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                            <div className={comm.pagination}>
+                                <ul>
+                                    <li className={comm.prev}>
+                                        <a href="javascript:void(0)">
+                                            <img src="/images/common/paging_arrow.svg" alt="" />
+                                        </a>
+                                    </li>
+                                    <li className={comm.numbers}>
+                                        <ul>
+                                            {/* pc버전 8개 까지 mo버전 5개 까지 */}
+                                            <li className={comm.active}><a href="javascript:void(0)">1</a></li>
+                                            <li><a href="javascript:void(0)">2</a></li>
+                                            <li><a href="javascript:void(0)">3</a></li>
+                                            <li><a href="javascript:void(0)">4</a></li>
+                                            <li><a href="javascript:void(0)">5</a></li>
+                                            <li><a href="javascript:void(0)">6</a></li>
+                                            <li><a href="javascript:void(0)">7</a></li>
+                                            <li><a href="javascript:void(0)">8</a></li>
+                                        </ul>
+                                    </li>
+                                    <li className={comm.prev}>
+                                        <a href="javascript:void(0)">
+                                            <img src="/images/common/paging_arrow.svg" alt="" />
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <Footer></Footer>
     </>;
 }
 
